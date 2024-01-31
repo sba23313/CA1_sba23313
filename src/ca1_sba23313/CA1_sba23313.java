688d8a447d22b90c42baaa4ab7621ef63a54fcf6
sba23313 <sba23313@student.cct.ie>
2024-01-31 22:17:31 +0000

    The method to write student data to a file
    This method writes the validated data to "status.txt. file"

--- src/ca1_sba23313/CA1_sba23313.java
+++ src/ca1_sba23313/CA1_sba23313.java
@@ -4,6 +4,9 @@
  */
 package ca1_sba23313;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 /**
  *
  * @author palic
@@ -15,6 +18,14 @@ public class CA1_sba23313 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    private static void writeDataToFile(String studentNumber, String secondName, int numberOfClasses) {
+        // Writes the validated data to "status.txt" file
+        String workload = typeWorkload(numberOfClasses);
+        try ( BufferedWriter writer = new BufferedWriter(new FileWriter("status.txt", true))) {
+            writer.write(studentNumber + " - " + secondName + "\n" + workload + "\n");
+        } catch (Exception e) {
+            System.out.println("An error occurred while writing to the file.");
+        }
     }
-    
+
 }
