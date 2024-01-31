d885024657a7c847a90e1bd070858a196755a5c6
sba23313 <sba23313@student.cct.ie>
2024-01-31 21:09:42 +0000

    Initial commit

---
+++ src/ca1_sba23313/CA1_sba23313.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca1_sba23313;
+
+/**
+ *
+ * @author palic
+ */
+public class CA1_sba23313 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
